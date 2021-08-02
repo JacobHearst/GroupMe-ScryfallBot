4228cd7a0c39cee0f22e7dda700c3e6ebc3751af
Jacob Hearst <jacob.hearst@jamf.com>
2021-08-02 14:30:33 -0500

    main Add ambiguous name handling

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
-var HTTPS = require('https');
-var Stream = require('stream').Transform
+var HTTPS = require("https");
+var Stream = require("stream").Transform
 
 var botID = process.env.BOT_ID;
 var accessToken = process.env.GM_TOKEN
 
 function respond() {
-    var request = JSON.parse(this.req.chunks[0])
-    var botRegex = /\[\[(.+)\]\]/;
+    const request = JSON.parse(this.req.chunks[0])
+    const botRegex = /\[\[(.+)\]\]/;
+    const match = request.text.match(botRegex)[1]
 
-    console.log(request)
 
-    if (request.text && botRegex.test(request.text)) {
+    if (request.text && match) {
         this.res.writeHead(200);
-        getCard(request.text.match(botRegex)[1], postMessage)
+        getCard(match, postCardDetails)
         this.res.end();
     } else {
         console.log("No card syntax found");
@@ -23,20 +23,21 @@ function respond() {
 
 function getCard(cardName, callback) {
     console.log(`Getting card with name: ${cardName}`)
-    const encodedName = encodeURI(cardName.replace(" ", "+"))
+    const encodedName = encodeURI(cardName)
 
     HTTPS.get(`https://api.scryfall.com/cards/named?fuzzy=${encodedName}`, (response) => {
-        let data = '';
+        let data = "";
 
-        // A chunk of data has been received.
-        response.on('data', (chunk) => {
+        response.on("data", (chunk) => {
             data += chunk
         })
 
-        response.on('end', () => {
+        response.on("end", () => {
             const responseBody = JSON.parse(data)
-            if (responseBody.status > 200) {
-                console.error(`Scryfall request failed: ${responseBody.details}`)
+            if (responseBody.status > 400) {
+                const text = `Scryfall request failed: ${responseBody.details}`
+                postMessage({ text })
+                console.error(text)
             } else {
                 callback(responseBody)
             }
@@ -50,91 +51,92 @@ function getCard(cardName, callback) {
         })
 }
 
-function uploadImageToGroupMe(uri, callback) {
-    HTTPS.get(uri, (response) => {
-        var data = new Stream();
-        response.on('data', (chunk) => { data.push(chunk) })
-        response.on('end', () => {
-            const options = {
-                hostname: 'image.groupme.com',
-                port: 443,
-                path: '/pictures',
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/jpeg',
-                    'X-Access-Token': accessToken
-                }
-            }
-
-            const req = HTTPS.request(options, res => {
-                let groupMeData = ""
-
-                res.on('data', d => {
-                    groupMeData += d
-                })
-
-                res.on('end', () => {
-                    callback(JSON.parse(groupMeData))
-                })
-            })
-
-            req.on('error', error => {
-                console.error(error)
-            })
-
-            req.write(data.read())
-            req.end()
-        })
-    })
-}
-
-function postMessage(cardData) {
-    let options = {
-        hostname: 'api.groupme.com',
-        path: '/v3/bots/post',
-        method: 'POST'
-    };
-
-    let body = {
-        "bot_id": botID,
-        "text": `View on Scryfall: ${cardData.scryfall_uri}`,
-    };
-
+function postCardDetails(cardData) {
     const image_uris = cardData.image_uris
     if (image_uris && (image_uris.normal || image_uris.large || image_uris.small)) {
         uploadImageToGroupMe(image_uris.normal || image_uris.large || image_uris.small, (response) => {
-            body.attachments = [
-                {
-                    "type": "image",
-                    "url": response.payload.picture_url
-                }
-            ]
+            body = {
+                text: `Scryfall: ${cardData.scryfall_uri}`,
+                attachments: [
+                    {
+                        "type": "image",
+                        "url": response.payload.picture_url
+                    }
+                ]
+            }
 
-            console.log('sending ' + body.text + ' to ' + botID);
-
-            botReq = HTTPS.request(options, function (res) {
-                let resData = ""
-                if (res.statusCode == 202) {
-                    //neat
-                } else {
-                    console.log('rejecting bad status code ' + res.statusCode);
-                }
-
-                res.on("data", (data) => resData += data)
-                res.on("end", () => console.log(resData))
-            });
-
-            botReq.on('error', function (err) {
-                console.log('error posting message ' + JSON.stringify(err));
-            });
-            botReq.on('timeout', function (err) {
-                console.log('timeout posting message ' + JSON.stringify(err));
-            });
-            botReq.end(JSON.stringify(body));
+            postMessage(body)
         })
     } else {
-        console.log(`No images for card: ${cardData.name}`)
+        const text = `No images for card: ${cardData.name}` 
+        console.warn(text)
+        postMessage({ text })
     }
+}
+
+function uploadImageToGroupMe(uri, callback) {
+    HTTPS.get(uri, (response) => {
+        var data = new Stream();
+        response.on("data", (chunk) => data.push(chunk))
+        response.on("end", () => postImage(data, callback))
+    })
+}
+
+function postImage(data, callback) {
+    const options = {
+        hostname: "image.groupme.com",
+        port: 443,
+        path: "/pictures",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/jpeg",
+            "X-Access-Token": accessToken
+        }
+    }
+
+    const req = HTTPS.request(options, res => {
+        let groupMeData = ""
+        res.on("data", data => groupMeData += data)
+
+        res.on("end", () => callback(JSON.parse(groupMeData)))
+    })
+
+    req.on("error", error => console.error(error))
+
+    req.write(data.read())
+    req.end()
+}
+
+function postMessage(content) {
+    let options = {
+        hostname: "api.groupme.com",
+        path: "/v3/bots/post",
+        method: "POST"
+    }
+
+    let body = {
+        bot_id: botID,
+        ...content
+    }
+
+    let request = HTTPS.request(options, function (res) {
+        let resData = ""
+        if (res.statusCode > 202) {
+            console.log("rejecting bad status code " + res.statusCode);
+        }
+
+        res.on("data", (data) => resData += data)
+        res.on("end", () => console.log(resData))
+    })
+
+    request.on("error", function (err) {
+        console.error("Errored posting message: " + JSON.stringify(err));
+    })
+    request.on("timeout", function (err) {
+        console.error("Timed out posting message: " + JSON.stringify(err));
+    })
+
+    request.end(JSON.stringify(body));
 }
 
 
